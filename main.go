7456ffeae59e8f870321beed0b20de1cc4937b72
Khalifah Khalil Shabazz <khalifahks@kohirens.com>
2020-12-18 00:19:57 -0500

    add: Errors when missing required arguments.

--- main.go
+++ main.go
@@ -4,48 +4,69 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
 )
 
 var verbosityLevel int = 0
-
-func main() {
-    // Ensure we exit with an error code and log message
-    // when needed after deferred cleanups have run.
-    // Credit: https://medium.com/@matryer/golang-advent-calendar-day-three-fatally-exiting-a-command-line-tool-with-grace-874befeb64a4
-    var err error
-    defer func() {
-        if err != nil {
-            log.Fatalln(err)
-        }
-    }()
-
-    //options := getArgs()
+var errMsgs = [...]string{
+	"please specify a path (or URL) to a template",
+	"enter a local path to output the app",
 }
 
-func getArgs() map[string]string {
-    options := make(map[string]string)
-    var tplPath string
-    var appPath string
-    var answers string
+func main() {
+	// Ensure we exit with an error code and log message
+	// when needed after deferred cleanups have run.
+	// Credit: https://medium.com/@matryer/golang-advent-calendar-day-three-fatally-exiting-a-command-line-tool-with-grace-874befeb64a4
+	var err error
+	defer func() {
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}()
+}
 
-    flag.StringVar(&answers, "answers", "", "Path to an answer file.")
+func getArgs() (map[string]string, error) {
+	var tplPath string
+	var appPath string
+	var answers string
+	var err error
+	options := make(map[string]string)
 
-    options["tplPath"] = flag.Arg(1)
-    options["appPath"] = flag.Arg(2)
+	verboseF(1, "no. arguments passed in: %d\n", len(os.Args))
+	verboseF(1, "arguments passed in: %v\n", os.Args)
 
-    verboseF(1,"downloading \"%v\"", tplPath)
-    verboseF(1, "will make \"%v\"",  appPath)
+	if len(os.Args) < 2 {
+		err = fmt.Errorf("display help")
+	}
 
-    if answers != "" {
-        options["answers"] = flag.Arg(2)
-        verboseF(1, "answer are located here: \"%v\"", answers)
-    }
+	flag.StringVar(&answers, "answers", "", "Path to an answer file.")
+	flag.IntVar(&verbosityLevel, "verbosity", 0, "extra detail processing info.")
 
-    return options
+	options["tplPath"] = flag.Arg(0)
+	options["appPath"] = flag.Arg(1)
+
+	verboseF(1, "downloading \"%v\"\n", tplPath)
+	verboseF(1, "will make \"%v\"\n", appPath)
+
+	if options["tplPath"] == "" {
+		err = fmt.Errorf(errMsgs[0])
+		return options, err
+	}
+
+	if options["appPath"] == "" {
+		err = fmt.Errorf(errMsgs[1])
+		return options, err
+	}
+
+	if answers != "" {
+		verboseF(1, "answer are located here: \"%v\"", answers)
+	}
+
+	return options, err
 }
 
 func verboseF(lvl int, message string, a ...interface{}) {
-    if verbosityLevel > lvl {
-        fmt.Printf(message, a...)
-    }
+	if verbosityLevel >= lvl {
+		fmt.Printf(message, a...)
+	}
 }
