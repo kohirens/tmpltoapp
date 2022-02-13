6a87dff439b8cf0624e699ffcabfeaa964c080b4
Khalifah Khalil Shabazz <shabazzk@gmail.com>
2022-02-12 21:20:14 -0500

    add: File for collective errors.

---
+++ errors.go
@@ -0,0 +1,11 @@
+package main
+
+var err = struct {
+	gettingAnswers,
+	missingTmplJson,
+	tmplManifest404 string
+}{
+	gettingAnswers:  "problem getting answers; error %q",
+	missingTmplJson: "%s is a file that is required to be in the template, there was a problem reading it; error %q",
+	tmplManifest404: "not %s found",
+}
