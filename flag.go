a6d4bc5536537436137dad2211b165025f1932c0
Khalifah Shabazz <619281+b01@users.noreply.github.com>
2022-06-03 10:53:44 -0400

    Clean up comment

--- flag.go
+++ flag.go
@@ -10,26 +10,6 @@
 	```shell
 	tmpltoapp [options] "<dir/url>" "<outputDir>" "<answers>"
 	```
-
-	## Description
-
-	Use a template to initialize a new project. A template can be a local directory
-	or a zip from a URL. Zip files will be downloaded and extracted to a local
-	directory.
-
-	## Options
-
-	**--tmplPath**, **-t** URL to a zip or a local path to a directory
-
-	**--answers**, **-a** Path to an answer file.
-
-	**--help**, **-h** Output this documentation.
-
-	**--verbosity** Control the level of information/feedback the program will
-	output to the user.
-
-	**--version**, **-v** Output version information.
-
 */
 package main
 
