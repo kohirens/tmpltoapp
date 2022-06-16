9a5d1334ff3f9b3be6bd78dc6da1f3b6b3f285f2
Khalifah Khalil Shabazz <shabazzk@gmail.com>
2022-06-16 07:52:42 -0400

    Extend config with define method.

--- flag.go
+++ flag.go
@@ -125,6 +125,23 @@ func defineFlags(programName string, handling flag.ErrorHandling) (flagStore *fl
 	return
 }
 
+// define All application flags.
+func (cf *Config) define() {
+	flag.StringVar(&cf.answersPath, "answers", "", usageMsgs["answers"])
+	flag.StringVar(&cf.answersPath, "a", "", usageMsgs["answers"]+" (shorthand)")
+	flag.StringVar(&cf.appPath, "appPath", "", usageMsgs["appPath"])
+	flag.StringVar(&cf.appPath, "p", "", usageMsgs["appPath"]+" (shorthand)")
+	flag.StringVar(&cf.branch, "branch", "", usageMsgs["branch"])
+	flag.BoolVar(&cf.help, "help", false, usageMsgs["help"])
+	flag.BoolVar(&cf.help, "h", false, usageMsgs["help"]+" (shorthand)")
+	flag.StringVar(&cf.tplPath, "tmplPath", "", usageMsgs["tmplPath"])
+	flag.StringVar(&cf.tplPath, "t", "", usageMsgs["tmplPath"]+" (shorthand)")
+	flag.StringVar(&cf.tmplType, "tmplType", "zip", usageMsgs["tmplType"])
+	flag.IntVar(&verbosityLevel, "verbosity", 0, usageMsgs["verbosity"])
+	flag.BoolVar(&cf.version, "version", false, usageMsgs["version"])
+	flag.BoolVar(&cf.version, "v", false, usageMsgs["version"]+" (shorthand)")
+}
+
 // flagMain Process and validate all CLI flags.
 func flagMain() error {
 	err1 := flagStore.Flags.Parse(os.Args[1:])
