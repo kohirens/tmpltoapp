5c022c23721753135708d207636b7d5a7763efcd
Khalifah Khalil Shabazz <shabazzk@gmail.com>
2022-06-26 17:03:10 -0400

    Template To App Release Candidate
    This and the previous changes present new addtion as well as breaking changes.
    
    The release should ensure that the program functionas as documented.
    
    rel: 1.0.0-rc

--- config.go
+++ config.go
@@ -14,7 +14,7 @@ type Config struct {
 	answersPath           string   // path to a file containing values to variables to be parsed.
 	appPath               string   // Location of the processed template output.
 	cacheDir              string
-	tmplPath              string    // TODO: Rename to tmplPath for consistency.
+	tmplPath              string    // URL or local path to a template.
 	tmpl                  string    // Path to template, this will be the cached path.
 	ExcludeFileExtensions []string  // Files to skip when sending to the go parsing engine.
 	IncludeFileExtensions []string  // Files to include when sending to the go parsing engine.
