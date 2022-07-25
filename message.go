1411577ca1aeda0a403f45f51fa5b2afb2c54d8d
Khalifah Khalil Shabazz <shabazzk@gmail.com>
2022-07-24 20:04:59 -0400

    chg: Add more feedback when user needs to add values for missing placeholders.

--- message.go
+++ message.go
@@ -11,3 +11,15 @@ var usageMsgs = map[string]string{
 	"verbosity":   "Set the level of information printed when running.",
 	"version":     "print build version information and exit 0.",
 }
+
+var messages = struct {
+	questionAnsweredWith  string
+	questionAnswerStat    string
+	questionHasAnAnswer   string
+	pleaseAnswerQuestions string
+}{
+	questionAnsweredWith:  "%q was answered with %q",
+	questionAnswerStat:    "there are %v placeholders and %v values",
+	questionHasAnAnswer:   "question %q already has an answer of %q, so skipping",
+	pleaseAnswerQuestions: "please answer the following\nnote: providing no value will render the placeholder with no valued when processing is done",
+}
