946d2a5831429e49cb664f7d41304879268ebe8f
Khalifah Khalil Shabazz <shabazzk@gmail.com>
2022-10-08 07:36:59 -0400

    Refactored Program Output/Feedback Functions

--- logging.go
+++ logging.go
@@ -7,10 +7,32 @@ const (
 	verboseLvlInfo = 1
 	verboseLvlWarn = 2
 	verboseLvlErr  = 3
-	verboseLvlDbug = 4
+	verboseLvlFat  = 4
+	verboseLvlDbug = 5
 )
 
 var verbosityLevel = verboseLvlLog
+
+func dbugf(message string, vars ...interface{}) {
+	verboseF(verboseLvlDbug, message, vars...)
+}
+
+func errf(message string, vars ...interface{}) {
+	verboseF(verboseLvlErr, message, vars...)
+}
+
+func fatf(message string, vars ...interface{}) {
+	verboseF(verboseLvlFat, message, vars...)
+}
+
+func infof(message string, vars ...interface{}) {
+	verboseF(verboseLvlInfo, message, vars...)
+}
+
+// logf Log all the time, useful for giving the user feedback on progress.
+func logf(message string, vars ...interface{}) {
+	verboseF(verboseLvlLog, message, vars...)
+}
 
 // Show additional logging based on the verbosity level. Prints a newline after every message.
 func verboseF(lvl int, message string, a ...interface{}) {
@@ -20,23 +42,6 @@ func verboseF(lvl int, message string, a ...interface{}) {
 	}
 }
 
-func infof(message string, vars ...interface{}) {
-	verboseF(verboseLvlInfo, message, vars...)
-}
-
 func warnf(message string, vars ...interface{}) {
 	verboseF(verboseLvlWarn, message, vars...)
-}
-
-func errf(message string, vars ...interface{}) {
-	verboseF(verboseLvlErr, message, vars...)
-}
-
-func dbugf(message string, vars ...interface{}) {
-	verboseF(verboseLvlDbug, message, vars...)
-}
-
-// logf Log all the time, useful for giving the user feedback on progress.
-func logf(message string, vars ...interface{}) {
-	verboseF(verboseLvlLog, message, vars...)
 }
